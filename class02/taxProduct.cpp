9e354d87c357ba082739379a2f0dd0ca38909625
Danilo Borges da Silva <daniloborges.cg@gmail.com>
2026-03-10 10:57:50 -0300

    código taxProduct atualizado

--- class02/taxProduct.cpp
+++ class02/taxProduct.cpp
@@ -5,9 +5,7 @@ using namespace std;
 const double TAXA_DE_JUROS_MENSAL = 0.015;
 const double VALOR_PARCELA = 50;
 
-void calculaMesesEJuros(double valorDoProduto) {
-    int meses = 0;
-    double valorTotalJuros = 0;
+void calculaMesesEJuros(double valorDoProduto, int &meses, double &valorTotalJuros) {
 
     while (valorDoProduto > 0) {
         double valorJurosAtual = valorDoProduto * TAXA_DE_JUROS_MENSAL;
@@ -17,14 +15,17 @@ void calculaMesesEJuros(double valorDoProduto) {
         meses++;
     }
 
-    cout << "Número de meses para pagar o produto: " << meses << endl;
-    cout << "Valor total pago em juros: " << valorTotalJuros << endl;
+    
 }
 
 
 
 int main(){
-    int valorDoProtuto = 1000;
-    calculaMesesEJuros(valorDoProtuto);
+    double valorDoProtuto = 1000;
+    int meses = 0;
+    double valorTotalJuros = 0;
+    calculaMesesEJuros(valorDoProtuto, meses, valorTotalJuros);
+    cout << "Número de meses para pagar o produto: " << meses << endl;
+    cout << "Valor total pago em juros: " << valorTotalJuros << endl;
     return 0;
 }
